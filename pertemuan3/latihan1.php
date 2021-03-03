58b2f04d9a8704e13e1965fb22507db2f6552e2c
MuchamadTaufik <muhamadtaufikm10@gmail.com>
2021-03-03 17:18:50 +0700

    menambahkan

--- pertemuan3/latihan1.php
+++ pertemuan3/latihan1.php
@@ -21,6 +21,17 @@ do {
 $i++;
 } while ($i < 5);
 
+//<table border="1" cellpadding="10" cellspacing="0">
+//<?php
+//cara 1
+//for ($i = 1; $i <= 3; $i++) {
+    //echo "<tr>";
+    //for ( $j = 1; $j <= 5; $j++) {
+        //echo "<td>$i,$j</td>";
+    //}
+    //echo "</tr>";
+//}
+
 ?>
 
 
@@ -32,25 +43,11 @@ $i++;
     <title>Latihan1</title>
 </head>
 <body>
-    
-    <table border="1" cellpadding="10" cellspacing="0">
-    <?php
-    //cara 1
-    for ($i = 1; $i <= 3; $i++) {
-        echo "<tr>";
-        for ( $j = 1; $j <= 5; $j++) {
-            echo "<td>$i,$j</td>";
-        }
-        echo "</tr>";
-    }
-    ?>
-    </table>
 
     <table border="1" cellpadding="10" cellspacing="0">
-    <?php
-    for($i = 1; $i >= 3; $i++) :?>
+    <?php for($i = 1; $i <= 5; $i++) :?>
     <tr>
-        <?php for($j = 1; $j >= 5; $j++) :?>
+        <?php for($j = 1; $j <= 5; $j++) :?>
         <td><?php echo "$i, $j"; ?></td>
         <?php endfor; ?>
     </tr>
