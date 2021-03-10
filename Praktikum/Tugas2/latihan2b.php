1b66e596ae2765e372f1cd94dfbe47f9b683a3e2
MuchamadTaufik <muhamadtaufikm10@gmail.com>
2021-03-11 01:57:57 +0700

    latihan2a

---
+++ Praktikum/Tugas2/latihan2b.php
@@ -0,0 +1,45 @@
+<?php
+/*
+    Muchamad Taufik Mulyadi
+    203040142
+    Jumat,13.00
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Latihan 2B</title>
+
+    <style>
+        .wrapper {
+            border: 1px solid black;
+            padding: 10px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <?php
+
+    $jawabanIsset = "Isset adalah = Berfungsi untuk memeriksa apakah suatu variabel sudah diatur atau belum. Fungsi isset juga mengembalikan false jika variabel pengujian berisi nilai NULL. <br><br>";
+    $jawabanEmpty = "Empty adalah = Berfungsi sebagai penanda suatu kondisi. Contohnya yang sering kita temukan adalah dalam pengisian suatu data, apakah data tersebut ada isinya atau tidak.";
+
+    function soal($style)
+    {
+        global $jawabanIsset, $jawabanEmpty;
+        echo $jawabanIsset, $jawabanEmpty;
+        return $style;
+    }
+
+    ?>
+
+    <div class="wrapper">
+        <?= soal(" "); ?>
+    </div>
+
+</body>
+
+</html>
