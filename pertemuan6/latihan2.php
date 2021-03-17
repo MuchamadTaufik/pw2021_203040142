2f2bee84aa5688634011f3ee9f4969a09c4f36fc
MuchamadTaufik <muhamadtaufikm10@gmail.com>
2021-03-18 04:58:03 +0700

    latihan

---
+++ pertemuan6/latihan2.php
@@ -0,0 +1,20 @@
+<?php
+/* 
+    Muchamad Taufik Mulyadi
+    203040142
+    https://github.com/MuchamadTaufik/pw2021_203040142
+    Pertemuan 6( 11 Maret 2021 )
+    Materi Minggu ini mempelajari mengenai Associative Array
+*/
+?>
+
+<?php
+// $mahasiswa = [
+//     ["Muchamad Taufik Mulyadi", "203040142", "TIF", "Muchamad Taufik Mulyadi"],
+//     ["Novyaaryn", "203040000", "TIF", "Novyaryn@gmail.com"],
+//     ["Staphalgar", "203044444", "TM", "Staph@gmail.com"]
+// ];
+
+// Array Associative
+// definisinya sama seperti array numerik, kecuali
+// key-nya adalah string yang kita buat sendiri
