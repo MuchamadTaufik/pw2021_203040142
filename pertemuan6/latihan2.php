962c148f2c228213d0523ba215bbf4894c0911a2
MuchamadTaufik <muhamadtaufikm10@gmail.com>
2021-03-19 13:43:40 +0700

    latihan

--- pertemuan6/latihan2.php
+++ pertemuan6/latihan2.php
@@ -18,3 +18,43 @@
 // Array Associative
 // definisinya sama seperti array numerik, kecuali
 // key-nya adalah string yang kita buat sendiri
+$mahasiswa = [
+    [
+        "nama" => "Novya Aryanti", 
+        "nrp" => "203040111",
+        "email" => "novyaryn@gmail.com",
+        "jurusan" => "Teknik Informatika",
+        "gambar" => "novya.png"
+    ],
+    [
+        "nama" => "Muhamad Taufik Mulyadi", 
+        "nrp" => "203040142",
+        "email" => "muhamadtaufikm10@gmail.com",
+        "jurusan" => "Teknik Informatika",
+        "gambar" => "taufik.png"
+    ]
+];
+
+?> 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Daftar Mahasiswa</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <?php foreach( $mahasiswa as $mhs ) : ?>
+        <ul>
+            <li>
+                <img src="gambar/<?= $mhs["gambar"]; ?>">
+            </li>
+            <li>Nama : <?= $mhs["nama"]; ?></li>
+            <li>NRP : <?= $mhs["nrp"]; ?></li>
+            <li>Jurusan : <?= $mhs["jurusan"]; ?></li>
+            <li>Email : <?= $mhs["email"]; ?></li>
+        </ul>
+    <?php endforeach; ?>
+
+
+</body>
+</html>
