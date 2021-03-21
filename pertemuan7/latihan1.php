08c755b3b126503e157bed305b41ffb28c316940
MuchamadTaufik <muhamadtaufikm10@gmail.com>
2021-03-21 22:38:10 +0700

    latihan

---
+++ pertemuan7/latihan1.php
@@ -0,0 +1,17 @@
+<?php
+/* 
+    Linggih Kusumah dilagga
+    203040174
+    github.com/galih250/pw2021_203040174
+    Pertemuan 7( 26 Februari 2021 )
+    Materi Minggu ini mempelajari mengenai GET & POST
+*/
+?>
+<?php 
+// Superglobals 
+// Variable global milik php
+// Termasuk array associative
+// $_GET 
+// bisa di tambahkan melalui URL
+// jika lebih dari 1 variable tambah &
+// var_dump($_GET);
